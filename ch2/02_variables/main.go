8069074f675932ab919dfc23803c5f18968e19d5
Ilya Golubkov <ilya.golubkov@gmail.com>
2020-02-19 08:06:57 +0600

    int variables

--- ch2/02_variables/main.go
+++ ch2/02_variables/main.go
@@ -8,4 +8,17 @@ func main() {
 
 	j := 2
 	fmt.Printf("%d - %T\n", j, j)
+
+	var k1 uint8 = 20
+	// k1 = 256 //complier error
+	//k1 = -2 //compiler error
+
+	fmt.Printf("%d - %T\n", k1, k1)
+
+	var k2 uint16 = 30
+
+	// k1 = k2 //compiler error
+	//k2 = k1 //compiler error
+
+	fmt.Printf("%d - %T\n", k2, k2)
 }
